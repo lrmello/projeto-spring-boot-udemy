df9f03fec2de805b4c3e667502ebf0bfe5d71054
Leonardo Reis <leonardo.reis28051995@gmail.com>
2024-10-11 14:30:12 -0300

    Configure ManyToMany for class Product and Category

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -5,19 +5,24 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
 
-	private static final long serialVersionUID = 1L; 
-	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -26,10 +31,12 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 	
-	@Transient
+	@JsonIgnore
+	@ManyToMany(cascade = CascadeType.PERSIST)
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
-	public Product() {	
+
+	public Product() {
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -101,7 +108,5 @@ public class Product implements Serializable {
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
 }
